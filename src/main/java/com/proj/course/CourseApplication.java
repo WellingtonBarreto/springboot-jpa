ae58d1d687c75be120fc0dbe26a1c5bc249de99b
Wellington Barreto <wellingtonalencarsantosb@gmail.com>
2024-11-25 08:25:56 -0300

    Primeiro commit

---
+++ src/main/java/com/proj/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.proj.course;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
